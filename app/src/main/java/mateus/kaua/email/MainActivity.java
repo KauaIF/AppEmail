aa3cd8398bea77ab7b53b5a399e1cc829a90bfc9
20201TIIMI0128 <kaua271203@gmail.com>
2023-04-05 08:46:18 -0300

    05/04/2023 08:46

--- app/src/main/java/mateus/kaua/email/MainActivity.java
+++ app/src/main/java/mateus/kaua/email/MainActivity.java
@@ -2,8 +2,15 @@ package mateus.kaua.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etEmail = findViewById(R.id.etEmail);
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                EditText etTexto = findViewById(R.id.etTexto);
+                String email = etEmail.getText().toString();
+                String assunto = etAssunto.getText().toString();
+                String Texto = etTexto.getText().toString();
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+                String[] emails = new String[] {email};
+                i.putExtra(Intent.EXTRA_EMAIL,emails);
+                i.putExtra(Intent.EXTRA_SUBJECT,assunto);
+                i.putExtra(Intent.EXTRA_TEXT,Texto);
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                }
+                catch (ActivityNotFoundException e){
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
