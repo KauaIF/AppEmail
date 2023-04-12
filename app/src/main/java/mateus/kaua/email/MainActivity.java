bcba625dc5d0d1b1cb49e189767de8428166a154
20201TIIMI0128 <kaua271203@gmail.com>
2023-04-12 08:22:22 -0300

    12/04/2023 08:22

--- app/src/main/java/mateus/kaua/email/MainActivity.java
+++ app/src/main/java/mateus/kaua/email/MainActivity.java
@@ -22,22 +22,30 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /* Pegando itens da view */
                 EditText etEmail = findViewById(R.id.etEmail);
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
+                /* pegando dados dos itens */
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String Texto = etTexto.getText().toString();
+                /* criando a intenção de mudar de efetuar algo */
                 Intent i = new Intent(Intent.ACTION_SENDTO);
+                /* Buscando apps com a função mailto "e-mail para" */
                 i.setData(Uri.parse("mailto:"));
+                /* adicionando os parâmetros */
                 String[] emails = new String[] {email};
                 i.putExtra(Intent.EXTRA_EMAIL,emails);
                 i.putExtra(Intent.EXTRA_SUBJECT,assunto);
                 i.putExtra(Intent.EXTRA_TEXT,Texto);
+                /* usando um try e catch para caso não existam apps com essa função no dispositivo */
                 try {
+                    /* abra uma aba para escolher o app */
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 }
                 catch (ActivityNotFoundException e){
+                    /* Mostrando um aviso que o dispositivo não possui apps que resolvam a solicitação */
                     Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_SHORT).show();
                 }
             }
